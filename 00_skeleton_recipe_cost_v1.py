348a0c57d5c55d731df559facbef05b13ad586c1
aaliyahtap <101297401+aaliyahtap@users.noreply.github.com>
2022-10-02 22:45:42 +1300

    Add files via upload

---
+++ 00_skeleton_recipe_cost_v1.py
@@ -0,0 +1,36 @@
+"""
+Recipe Cost Calculator
+Skeleton v1
+Aaliyah T
+
+"""
+
+
+# set up functions**********************************************
+
+# function to check for integers. will loop until an integer is input
+def number_checker():
+    print("placeholder")
+
+
+# function to check string is within an allowable list. Will loop until an allowable word is input
+def string_checker(list_of_allowable):
+    print("placeholder")
+
+def not_blank()
+# set up lists and constants************************************
+calc_total_cost = []
+yes_no_list = [["yes", "y"], ["no", "n"]]
+chosen_recipe_list = []
+ingredients_list = []
+max_list_length = 10
+#  main***************************************************
+while len(chosen_recipe_list) != max_list_length:
+    print("placeholder")
+    # ask users for input
+    print("**")
+    print("** Welcome to my recipe cost calculator")
+    recipe= input("What recipe are you going to need the cost for? ")
+    chosen_recipe_list.append(recipe)
+
+
